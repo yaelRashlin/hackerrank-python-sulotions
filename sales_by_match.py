5d6b66a38219fd79b5fc6111d01952e2009fddf8
Yael Rashlin <58134106+yaelRashlin@users.noreply.github.com>
2021-06-29 22:43:12 +0300

    Create sales_by_match.py

---
+++ sales_by_match.py
@@ -0,0 +1,40 @@
+'''
+https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from math import floor
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    t = {}
+    for i in ar:
+        t.setdefault(i, 0)
+        t[i] = t[i] + 1
+    return sum([floor(i / 2) for i in t.values() if i > 1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
