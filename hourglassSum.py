11f9a59c1e8faecf4bcc75b063fc04fe0e9223b7
Yael Rashlin <58134106+yaelRashlin@users.noreply.github.com>
2021-07-15 00:41:20 +0300

    Create hourglassSum.py

---
+++ hourglassSum.py
@@ -0,0 +1,40 @@
+'''
+https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+'''
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    t = []
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if i + 2 < len(arr) and j + 2 < len(arr):
+                t.append(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+    return max(t)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in xrange(6):
+        arr.append(map(int, raw_input().rstrip().split()))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
